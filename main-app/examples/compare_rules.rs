4a1b67c0de28cd65b39f1e4f5bab9c8368c0d170
Andrew Conway <arcgit@greatcactus.org>
2021-12-29 23:49:16 +1100

    Look at possible changes to election outcomes

---
+++ main-app/examples/compare_rules.rs
@@ -0,0 +1,27 @@
+// Copyright 2021 Andrew Conway.
+// This file is part of ConcreteSTV.
+// ConcreteSTV is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// ConcreteSTV is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+// You should have received a copy of the GNU Affero General Public License along with ConcreteSTV.  If not, see <https://www.gnu.org/licenses/>.
+
+//! Compare the effects of different rules on NSW elections 2021.
+
+use act::ACTPre2020;
+use stv::compare_rules::CompareRules;
+use federal::FederalRules;
+use nsw::{NSWECLocalGov2021, NSWLocalCouncilLegislation2021MyGuessAtHighlyAmbiguousLegislation};
+use nsw::parse_lge::get_nsw_lge_data_loader_2021;
+use stv::parse_util::{FileFinder, RawDataSource};
+
+/// Compare various rules for the NSW2021 elections.
+fn main()  -> anyhow::Result<()> {
+    let loader = get_nsw_lge_data_loader_2021(&FileFinder::find_ec_data_repository())?;
+
+    let electorates = loader.all_electorates();
+    let iterator = electorates.iter().filter(|e|!e.ends_with(" Mayoral")).map(|e|loader.load_cached_data(e));
+
+    let comparer = CompareRules{ dir: "Comparison/NSW2021".to_string() };
+    comparer.compare_datasets::<usize,NSWECLocalGov2021,NSWLocalCouncilLegislation2021MyGuessAtHighlyAmbiguousLegislation,ACTPre2020,FederalRules,_>(iterator)?;
+
+    Ok(())
+}
---
+++ nsw/examples/compare_to_without_ivote.rs
@@ -0,0 +1,51 @@
+// Copyright 2021 Andrew Conway.
+// This file is part of ConcreteSTV.
+// ConcreteSTV is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+// ConcreteSTV is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
+// You should have received a copy of the GNU Affero General Public License along with ConcreteSTV.  If not, see <https://www.gnu.org/licenses/>.
+
+
+use std::collections::HashSet;
+use nsw::NSWECLocalGov2021;
+use nsw::parse_lge::get_nsw_lge_data_loader_2021;
+use stv::compare_transcripts::{DeltasInCandidateLists, DifferentCandidateLists};
+use stv::distribution_of_preferences_transcript::Transcript;
+use stv::election_data::ElectionData;
+use stv::parse_util::{FileFinder, RawDataSource};
+use stv::preference_distribution::{distribute_preferences, PreferenceDistributionRules};
+
+fn plain_count<R:PreferenceDistributionRules>(data : &ElectionData) -> Transcript<R::Tally> {
+    distribute_preferences::<R>(data,data.metadata.vacancies.unwrap(),&data.metadata.excluded.iter().cloned().collect::<HashSet<_>>(),&data.metadata.tie_resolutions,false)
+}
+
+/// Compare the "official" website results vs. the results of counting vs the results if iVote votes are not included.
+fn main() -> anyhow::Result<()> {
+    let loader = get_nsw_lge_data_loader_2021(&FileFinder::find_ec_data_repository())?;
+    let mut num_electorate = 0;
+    let mut num_different_official = 0;
+    let mut num_different_ivotes = 0;
+    for electorate in loader.all_electorates() {
+        num_electorate+=1;
+        let data_with_ivotes = loader.read_raw_data_possibly_rejecting_some_types(&electorate,None)?;
+        let data_without_ivotes = loader.read_raw_data_possibly_rejecting_some_types(&electorate,Some(vec!["iVote".to_string()].into_iter().collect()))?;
+        let total_num_votes = data_with_ivotes.num_votes();
+        let total_num_ivotes = total_num_votes-data_without_ivotes.num_votes();
+        let turnout = if let Some(enrolment) = data_with_ivotes.metadata.enrolment { format!(" enrolment {} informal {} turnout {:.1}%",enrolment.0,data_with_ivotes.informal,100.0*(total_num_votes+data_with_ivotes.informal) as f64/enrolment.0 as f64) } else { "".to_string() };
+        println!("Electorate {} {} formal votes including {} formal iVotes ({:.1}%){}",&electorate,total_num_votes,total_num_ivotes,100.0*total_num_ivotes as f64/total_num_votes as f64,turnout);
+        let transcript_with_ivotes = plain_count::<NSWECLocalGov2021>(&data_with_ivotes);
+        let transcript_without_ivotes = plain_count::<NSWECLocalGov2021>(&data_without_ivotes);
+        let compare_official : DeltasInCandidateLists = DifferentCandidateLists{ list1: data_with_ivotes.metadata.results.as_ref().unwrap().clone(), list2: transcript_with_ivotes.elected.clone() }.into();
+        if !compare_official.is_empty() {
+            println!("  Different to official results for {} : {}",&electorate,compare_official.pretty_print(&data_with_ivotes.metadata));
+            num_different_official+=1;
+        }
+        let compare_without_ivote  : DeltasInCandidateLists = DifferentCandidateLists{ list1: transcript_with_ivotes.elected.clone(), list2: transcript_without_ivotes.elected.clone() }.into();
+        if !compare_without_ivote.is_empty() {
+            println!("  Different when excluding iVotes for {} : {}",&electorate,compare_without_ivote.pretty_print(&data_with_ivotes.metadata));
+            num_different_ivotes+=1;
+        }
+        // let difference = stv::compare_transcripts::compare_transcripts(&transcript_with_ivotes,&transcript_without_ivotes);
+    }
+    println!("Of {} electorates, {} differ from official results and {} differ when iVote votes are removed",num_electorate,num_different_official,num_different_ivotes);
+    Ok(())
+}
